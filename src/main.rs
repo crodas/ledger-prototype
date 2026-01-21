c7b3cc7069ac74c94502d06eedbb069a8979fd3d
Cesar Rodas <cesar@rodasm.com.py>
2026-01-20 22:33:54 -0300

    Working on binary

--- src/main.rs
+++ src/main.rs
@@ -2,8 +2,33 @@ use std::env;
 use std::error::Error;
 use std::fs::File;
 
-use csv::Reader;
-use ledger::Ledger;
+use csv::{Reader, Trim};
+use ledger::{AccountId, Ledger, Reference};
+use serde::Deserialize;
+
+#[derive(Deserialize, Clone, Debug)]
+enum Action {
+    #[serde(rename = "deposit")]
+    Deposit,
+    #[serde(rename = "withdrawal")]
+    Withdrawal,
+    #[serde(rename = "dispute")]
+    Dispute,
+    #[serde(rename = "resolve")]
+    Resolve,
+    #[serde(rename = "chargeback")]
+    Chargeback,
+}
+
+#[derive(Deserialize, Clone, Debug)]
+struct CsvEntry {
+    #[serde(rename = "type")]
+    typ: Action,
+    client: AccountId,
+    tx: Reference,
+    #[serde(default)]
+    amount: Option<f64>,
+}
 
 fn main() -> Result<(), Box<dyn Error>> {
     let args: Vec<String> = env::args().collect();
@@ -12,13 +37,21 @@ fn main() -> Result<(), Box<dyn Error>> {
         std::process::exit(1);
     }
 
-    let file = File::open(&args[1])?;
-    let mut reader = Reader::from_reader(file);
+    let mut reader = csv::ReaderBuilder::new()
+        .trim(Trim::All) // <-- trims leading & trailing whitespace
+        .from_path(&args[1])?;
 
     let _ledger = Ledger::default();
 
-    for result in reader.records() {
-        let record = result?;
+    for (line, result) in reader.deserialize::<CsvEntry>().enumerate() {
+        let record = match result {
+            Ok(result) => result,
+            Err(err) => {
+                println!("Failed to parse line {}: {:?}", line, err);
+                continue;
+            }
+        };
+
         // TODO: parse record and call ledger methods
         println!("{:?}", record);
     }
